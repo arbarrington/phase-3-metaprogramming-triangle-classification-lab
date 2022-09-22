3ba1a00884bb7f966932fb339aa52bca575f81bb
Austin Barrington <arbarrington12@gmail.com>
2022-09-21 21:29:19 -0600

    commit tree e8ffb80d408b0aa717a88d184a01bdb366f58461

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
   # write code here
+  attr_reader :side1, :side2, :side3
+
+  def initialize side1, side2, side3
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  class TriangleError < StandardError
+  end
+
+  def kind
+    validate_triangle
+    if side1 == side2 && side2 == side3 && side1 > 0
+      :equilateral
+    elsif side1 == side2 || side1 == side3 || side2 == side3
+      :isosceles
+    elsif side1 != side2 && side2 != side3 && side1 != side3
+      :scalene
+    end
+  end
+
+  def validate_triangle
+    raise TriangleError unless valid_triangle_inequality?
+  end
+
+  def valid_triangle_inequality?
+    side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1
+  end
 end
